f31f3140bfb768452219f574020a738fd1997e46
Felipe Balbi <felipe@balbi.sh>
2020-12-28 19:40:36 +0200

    Machine Language: Chapter 3
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ ch3/ch3.s
@@ -0,0 +1,39 @@
+	CHROUT	:= $ffd2	; Output a character on the screen
+	GETIN	:= $ffe4	; Get a character from keyboard
+	STOP	:= $ffe1	; Check RUN/STOP key
+
+	.segment "CODE"
+	.proc main
+
+	lda #$93		; Clear Screen character
+	jsr CHROUT
+again:	
+	jsr STOP
+	beq exit
+	jsr GETIN
+
+trynum:	
+	cmp #$30
+	bcc again
+	cmp #$39
+	bcs tryalpha
+	jmp printchar
+
+tryalpha:
+	cmp #$41
+	bcc again
+	cmp #$5a
+	bcs again
+	jmp printchar
+
+printchar:
+	;; ora #$80		; Turn on highest bit. Prints shifted char
+	;; and #$fe		; Clear lowest bit. For digits,
+				; prints only evens
+	jsr CHROUT
+	jmp again
+
+exit:	
+	rts
+	.endproc
+
