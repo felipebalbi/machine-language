600b71137e9f72e7ae90652bf41b5313926b57d7
Felipe Balbi <felipe@balbi.sh>
2021-01-01 17:57:58 +0200

    Machine Language: ch5: clear screen with indirect indexed
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- ch5/clear-screen.s
+++ ch5/clear-screen.s
@@ -2,13 +2,26 @@
 
 	;; Clear the screen by writing spaces to the entire screen
 	.proc main
+	ldx $0288		; Check screen memory page address
+	stx $bc			; Store to $bc
+	inx			; Increment high byte
+	stx $be			; Store to $be
+	inx			; Increment again
+	stx $fd			; Store to $c0
+	stx $ff			; Store to $c2 as well
+
+	ldy #$e8		; Load low byte of top-most chunk
+	sty $fe			; Store to $c1
+	ldy #$00		; Initialize Y index
+	sty $bb			; Store to $bb
+	sty $bd			; Store to $bd
+	sty $fc			; Store to $bf
 	lda #$20		; Load SPACE into Accumulator
-	ldx #$00		; Initialize X index
 loop:
-	sta $0400,x		; Store to first chunk
-	sta $0500,x		; Store to second chunk
-	sta $0600,x		; Store to third chunk
-	sta $06e8,x		; Store to fourth chunk, but this is
+	sta ($bb),y		; Store to first chunk
+	sta ($bd),y		; Store to second chunk
+	sta ($fc),y		; Store to third chunk
+	sta ($fe),y		; Store to fourth chunk, but this is
 				; special. Screen RAM is 1000 bytes
 				; long, because of that we can use
 				; increments of $0100 for every chunk
@@ -18,8 +31,9 @@ loop:
 				; overlap of 24 bytes. There may be
 				; better ways to do this, but this
 				; works nicely.
-	inx			; Increment X index
+	iny			; Increment Y index
 	bne loop		; Continue going until X wraps around
 
 	rts
 	.endproc
+
